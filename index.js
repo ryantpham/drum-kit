e703a09a264dac57051d2d53b9b21c1b0fbb9849
Ryan Pham <rtpham10@gmail.com>
2024-02-28 20:14:19 -0800

    Add interactive drum kit feature with button-specific sounds

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-function handleClick(){
-    alert("I got Clicked")
-}
+//Global for Different Audios
+const audioW = new Audio('sounds/tom-1.mp3');
+const audioA = new Audio('sounds/tom-2.mp3');
+const audioS = new Audio('sounds/tom-3.mp3');
+const audioD = new Audio('sounds/tom-4.mp3');
+const audioJ = new Audio('sounds/snare.mp3');
+const audioK = new Audio('sounds/crash.mp3');
+const audioL = new Audio('sounds/kick-bass.mp3');
 
-
-const audio = new Audio('sounds/tom-3.mp3');
+//play sound when clicked on
 const buttons = document.querySelectorAll('button');
+
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        audio.play();
-    });
+    if(button.innerText == "w"){
+        button.addEventListener('click', () =>{
+            return audioW.play();
+        });
+    } else if (button.innerText == "a"){
+        button.addEventListener('click', ()=>{
+            return audioA.play();
+        });
+    } else if (button.innerText == "s"){
+        button.addEventListener('click', ()=>{
+            return audioS.play();
+        });
+    } else if (button.innerText == "d"){
+        button.addEventListener('click', ()=>{
+            return audioD.play();
+        });
+    } else if (button.innerText == "j"){
+        button.addEventListener('click', () =>{
+            return audioJ.play();
+        });
+    } else if (button.innerText == "k"){
+        button.addEventListener('click', () =>{
+            return audioK.play();
+        });
+    //audio L
+    } else {
+        button.addEventListener('click', () =>{
+            return audioL.play();
+        });
+    }
+
 });
